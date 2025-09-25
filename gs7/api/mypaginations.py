67ad8289c74c0fe3600f2c66baa501b5d0d54ba7
harsh-kikani <harsh.kikani@exhibytesolution.com>
2025-09-25 14:12:31 +0530

    limitoffset and cursor pagination

--- gs7/api/mypaginations.py
+++ gs7/api/mypaginations.py
@@ -1,9 +1,24 @@
-from rest_framework.pagination import PageNumberPagination
+# from rest_framework.pagination import PageNumberPagination
 
-class MyPageNumberPagination(PageNumberPagination):
-    page_size = 5
-    page_query_param = 'p'
-    page_size_query_param = 'records'
-    max_page_size = 7
-    last_page_strings = 'end'
+# class MyPageNumberPagination(PageNumberPagination):
+#     page_size = 5
+#     page_query_param = 'p'
+#     page_size_query_param = 'records'
+#     max_page_size = 7
+#     last_page_strings = 'end'
+    
+    
+# from rest_framework.pagination import LimitOffsetPagination
+
+# class MyLimitOffsetPagination(LimitOffsetPagination):
+#     default_limit = 5
+#     limit_query_param = 'mylimit'
+#     offset_query_param = 'myoffset'
+#     max_limit = 6
+
+from rest_framework.pagination import CursorPagination
+
+class MyCursorPagination(CursorPagination):
+    page_size = 3
+    ordering = 'name'
     
